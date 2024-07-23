6d378a20c1d2cb37132cecc964dedb10ec0d2c9a
SamhithaGunnam <113322185+SamhithaGunnam@users.noreply.github.com>
2024-07-23 12:38:31 -0400

    Update api.js

--- food-sharing-frontend/src/api.js
+++ food-sharing-frontend/src/api.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'http://3.22.224.142:8000/',
   timeout: 5000,
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Token ${localStorage.getItem('token')}`,
-    },
+  headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Token ${localStorage.getItem('token')}`,
+  },
 });
 
-export default api;
